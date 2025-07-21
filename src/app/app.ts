7ec91347b96d7aaef5ebae0d42380a9f88f14cd4
Ivan-M <sirius.ivo@gmail.com>
2025-07-21 01:48:06 -0300

    feature: integrate student-table and fetch student data

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { Toolbar } from './toolbar/toolbar';
 import { Navbar } from './navbar/navbar';
+import { HttpClient } from '@angular/common/http';
+import { Student } from '../shared/entities';
+import { StudentTable } from "./student-table/student-table";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule, Toolbar, Navbar],
+  imports: [RouterOutlet, CommonModule, Toolbar, Navbar, StudentTable],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
-  protected title = 'entregable-1';
+export class App implements OnInit {
+  
+  students: Student[] = [];
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<Student[]>('mocks/students.json').subscribe(data => {
+      this.students = data;
+    });
+  }
 }
