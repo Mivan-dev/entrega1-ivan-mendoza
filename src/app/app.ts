b773fa8e0cea7d5f4dfecb343653065bd4f92af2
Ivan-M <sirius.ivo@gmail.com>
2025-07-20 22:12:38 -0300

    feature: add imports to app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Toolbar } from './toolbar/toolbar';
+import { Navbar } from './navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, Toolbar, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
