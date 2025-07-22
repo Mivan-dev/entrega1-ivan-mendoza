8f7dfbef059e2deb6fe92a2653f51074d8a47471
Ivan-M <sirius.ivo@gmail.com>
2025-07-21 22:27:37 -0300

    feature: implement add-form component

---
+++ src/app/add-form/add-form.ts
@@ -0,0 +1,35 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Student } from '../../shared/entities';
+
+@Component({
+  selector: 'app-add-form',
+  imports: [ReactiveFormsModule, CommonModule],
+  templateUrl: './add-form.html',
+  styleUrl: './add-form.scss'
+})
+export class AddForm implements OnInit {
+
+  studentForm! : FormGroup;
+  @Output() studentAdded = new EventEmitter<Student>();
+  constructor(private fb: FormBuilder) { }
+  
+  ngOnInit() {
+    this.studentForm = this.fb.group({
+      name: ['', Validators.required],
+      surname: ['', Validators.required],
+      dni: ['', Validators.required],
+      age: ['', [Validators.required, Validators.min(0)]],
+      average: ['', [Validators.required, Validators.min(0), Validators.max(10)]],
+    });
+  }
+
+  onSubmit() {
+    this.studentAdded.emit(this.studentForm.value);
+  } 
+
+  onReset() {
+    this.studentForm.reset();
+  }
+}
