5178492dd3b2e661ab3d8e569426b08c4e656658
Ivan-M <sirius.ivo@gmail.com>
2025-07-22 18:37:33 -0300

    feature: Implementation of Snackbar Toast for Action Confirmation Messages using Angular Material

--- src/app/add-form/add-form.ts
+++ src/app/add-form/add-form.ts
@@ -1,18 +1,23 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, inject, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Student } from '../../shared/entities';
 import { Boldtitle } from '../../shared/directives/boldtitle';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-add-form',
-  imports: [ReactiveFormsModule, CommonModule, Boldtitle, NgbModule],
+  imports: [ReactiveFormsModule, CommonModule, Boldtitle, NgbModule, MatFormFieldModule, MatInputModule, MatButtonModule],
   templateUrl: './add-form.html',
   styleUrl: './add-form.scss'
 })
 export class AddForm implements OnInit {
 
+  private _snackBar = inject(MatSnackBar);
   studentForm! : FormGroup;
   @Output() studentAdded = new EventEmitter<Student>();
   constructor(private fb: FormBuilder) { }
@@ -29,10 +34,19 @@ export class AddForm implements OnInit {
 
   onSubmit() {
     this.studentAdded.emit(this.studentForm.value);
-    this.studentForm.reset();
+     if (this.studentForm.valid) {
+      this.showSuccessAdd();
+      this.studentForm.reset();
   } 
+}
 
   onReset() {
     this.studentForm.reset();
+  }
+
+  showSuccessAdd(){
+      const message = 'Estudiante agregado correctamente';
+      const action = 'Close';
+      this._snackBar.open(message, action);
   }
 }
--- src/app/delete-form/delete-form.ts
+++ src/app/delete-form/delete-form.ts
@@ -1,14 +1,19 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, inject, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-delete-form',
-  imports: [ReactiveFormsModule, CommonModule],
+  imports: [ReactiveFormsModule, CommonModule, MatFormFieldModule, MatInputModule, MatButtonModule],
   templateUrl: './delete-form.html',
   styleUrl: './delete-form.scss'
 })
 export class DeleteForm implements OnInit {
+  private _snackBar = inject(MatSnackBar);
   @Output() studentDeleted = new EventEmitter<string>();
 
   deleteStudentForm! : FormGroup;
@@ -24,10 +29,17 @@ export class DeleteForm implements OnInit {
 
   onDelete() {
     if (this.deleteStudentForm.valid) {
+      this.showSuccessDelete();
       const { dni } = this.deleteStudentForm.value;
       this.studentDeleted.emit(dni);
       this.deleteStudentForm.reset();
     }
+  };
 
-  }
+    showSuccessDelete(){
+      const message = 'Estudiante eliminado correctamente';
+      const action = 'Close';
+      this._snackBar.open(message, action);
+    }
+  
 }
