cfedb3158992416f85fb4c8e05af0a4d88af5f1e
Ivan-M <sirius.ivo@gmail.com>
2025-08-05 03:58:58 -0300

    feature: add actions in student-table component

--- src/app/student-table/student-table.ts
+++ src/app/student-table/student-table.ts
@@ -2,10 +2,11 @@ import { Component, Input } from '@angular/core';
 import { Student } from '../../shared/entities';
 import { MatTableModule } from '@angular/material/table';
 import { FullnamePipe } from '../../shared/pipes/fullname-pipe';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-student-table',
-  imports: [MatTableModule, FullnamePipe],
+  imports: [MatTableModule, FullnamePipe, RouterModule],
   templateUrl: './student-table.html',
   styleUrl: './student-table.scss'
 })
@@ -13,5 +14,13 @@ export class StudentTable {
   
   @Input() students: Student[] = [];
 
-  displayedColumns: string[] = ['fullname', 'age', 'dni', 'average'];
+  displayedColumns: string[] = ['fullname', 'age', 'dni', 'average', 'actions'];
+
+  constructor (private router: Router) { }
+
+  viewDetails(student: Student) {
+    this.router.navigate(['/ver-alumno'],{
+      state: { student }
+  });
+  }
 }
