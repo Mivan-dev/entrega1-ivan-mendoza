5deb9978f7a7781a1fb2c1b85d61ac98ff7e4eb2
Ivan-M <sirius.ivo@gmail.com>
2025-08-05 16:20:45 -0300

    feature: add cursos function

--- src/app/feature/cursos/cursos.ts
+++ src/app/feature/cursos/cursos.ts
@@ -1,11 +1,39 @@
 import { Component } from '@angular/core';
+import { Course } from '../../../shared/entities';
+import { CursosAPI} from './cursos-api';
+import { switchMap } from 'rxjs';
+import { CommonModule, JsonPipe } from '@angular/common';
+import { CursoTabla } from './curso-tabla/curso-tabla';
 
 @Component({
   selector: 'app-cursos',
-  imports: [],
+  imports: [CommonModule, JsonPipe, CursoTabla],
   templateUrl: './cursos.html',
   styleUrl: './cursos.scss'
 })
 export class Cursos {
-
+  cursos!: Course[]
+    constructor(private cursosApi: CursosAPI) { } 
+  
+    ngOnInit() {
+      this.cursosApi.getCursos().subscribe(cursos => {
+        this.cursos = cursos;
+      });
+    }
+  
+    deleteCourse(course: Course) {
+      this.cursosApi.deleteCurso(course).subscribe(()=> {
+        this.cursosApi.getCursos().subscribe(cursos => {
+          this.cursos = cursos;
+        })
+      });
+  
+      this.cursosApi.deleteCurso(course).pipe(
+        // Despues que termina el delete, vuelve a obtener los alumnos
+        switchMap(() => this.cursosApi.getCursos())
+      ).subscribe(cursos => {
+        this.cursos = cursos;
+      } );
+  
+    }
 }
