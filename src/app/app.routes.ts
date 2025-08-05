6f1b25db59ce90984593c7a0d5370a453283d17e
Ivan-M <sirius.ivo@gmail.com>
2025-08-05 02:18:58 -0300

    feature: add route paths and implement lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { Alumnos } from './feature/alumnos/alumnos';
+import { Cursos } from './feature/cursos/cursos';
+import { Inscripciones } from './feature/inscripciones/inscripciones';
+import { RoutesPaths } from '../shared/routes';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: Alumnos
+    },
+    {
+        path: RoutesPaths.ALUMNOS,
+        component: Alumnos
+    },
+    {
+        path: RoutesPaths.CURSOS,
+        loadComponent: () => import('./feature/cursos/cursos').then(m => m.Cursos)
+    },
+    {
+        path: RoutesPaths.INSCRIPCIONES,
+        loadComponent: () => import('./feature/inscripciones/inscripciones').then(m => m.Inscripciones)
+    },
+    {
+        path: '**',
+        component: Alumnos
+    }
+];
