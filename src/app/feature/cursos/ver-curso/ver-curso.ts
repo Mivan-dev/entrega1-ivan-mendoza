27332facc8c37c90c660a69b261768aa5aac28dd
Ivan-M <sirius.ivo@gmail.com>
2025-08-05 16:33:15 -0300

    feature: update view and logic in ver-curso component

--- src/app/feature/cursos/ver-curso/ver-curso.ts
+++ src/app/feature/cursos/ver-curso/ver-curso.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { Course } from '../../../../shared/entities';
+import { Router } from '@angular/router';
+import { CommonModule, JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-ver-curso',
-  imports: [],
+  imports: [JsonPipe, CommonModule],
   templateUrl: './ver-curso.html',
   styleUrl: './ver-curso.scss'
 })
 export class VerCurso {
+  course: Course | undefined;
 
+  constructor(private router: Router){
+    const navigation = this.router.getCurrentNavigation();
+    this.course = navigation?.extras.state?.['course'];
+    
+  }
 }
