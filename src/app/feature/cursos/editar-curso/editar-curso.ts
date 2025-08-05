9d53d2c0ffb04b0c1de0615cb081a4c4ebd5f39b
Ivan-M <sirius.ivo@gmail.com>
2025-08-05 16:15:51 -0300

    feature: add editar-curso component

---
+++ src/app/feature/cursos/editar-curso/editar-curso.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-editar-curso',
+  imports: [],
+  templateUrl: './editar-curso.html',
+  styleUrl: './editar-curso.scss'
+})
+export class EditarCurso {
+
+}
