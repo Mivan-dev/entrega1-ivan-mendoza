487d79ca545d5548b7d7308fc3ef30a9bc01a5e9
Ivan-M <sirius.ivo@gmail.com>
2025-08-18 19:22:28 -0300

    refactor: reorganize components in layout folder

---
+++ src/app/layout/toolbar/toolbar.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Boldtitle } from '../../../shared/directives/boldtitle';
+
+@Component({
+  selector: 'app-toolbar',
+  imports: [Boldtitle],
+  templateUrl: './toolbar.html',
+  styleUrl: './toolbar.scss'
+})
+export class Toolbar {
+
+}
