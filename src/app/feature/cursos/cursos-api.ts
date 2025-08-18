dfa1b86853b119a81a1fec4df03e9f1a2cff7a4a
Ivan-M <sirius.ivo@gmail.com>
2025-08-18 16:42:44 -0300

    feature: implements mockApi Courses

--- src/app/feature/cursos/cursos-api.ts
+++ src/app/feature/cursos/cursos-api.ts
@@ -7,25 +7,26 @@ import { Course } from '../../../shared/entities';
   providedIn: 'root'
 })
 export class CursosAPI {
-  baseUrl: string = 'http://localhost:3000';
+  // baseUrl: string = 'http://localhost:3000';
+  baseUrl: string = 'https://68a005ac6e38a02c581795b4.mockapi.io/api/v1/Courses';
 
   constructor(
     private http: HttpClient
   ) { }
 
   getCursos(): Observable<Course[]> {
-    return this.http.get<Course[]>(`${this.baseUrl}/courses`).pipe(delay(500));
+    return this.http.get<Course[]>(`${this.baseUrl}`);
   }
 
   getCursoPorId(id: string): Observable<Course> {
-  return this.http.get<Course>(`${this.baseUrl}/courses/${id}`).pipe(delay(500));
+  return this.http.get<Course>(`${this.baseUrl}/${id}`);
   }
 
   deleteCurso(courses: Course): Observable<void> {
-    return this.http.delete<void>(`${this.baseUrl}/courses/${courses.id}`).pipe(delay(500));
+    return this.http.delete<void>(`${this.baseUrl}/${courses.id}`);
   }
 
   updateCurso(courses: Course): Observable<Course> {
-  return this.http.put<Course>(`${this.baseUrl}/courses/${courses.id}`, courses).pipe(delay(500));
+  return this.http.put<Course>(`${this.baseUrl}/${courses.id}`, courses);
   }
 }
