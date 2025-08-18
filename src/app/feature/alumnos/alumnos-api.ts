31575914f713e44fb8c482075079957c3bb16453
Ivan-M <sirius.ivo@gmail.com>
2025-08-18 16:41:24 -0300

    feature:  implements mockApi

--- src/app/feature/alumnos/alumnos-api.ts
+++ src/app/feature/alumnos/alumnos-api.ts
@@ -7,26 +7,27 @@ import { Student } from '../../../shared/entities';
   providedIn: 'root'
 })
 export class AlumnosAPI {
-  baseUrl: string = 'http://localhost:3000';
+  //baseUrl: string = 'http://localhost:3000'; // Si usas json-server local, adaptar paths
+  baseUrl: string = 'https://68a005ac6e38a02c581795b4.mockapi.io/api/v1/students';
 
   constructor(
     private http: HttpClient
   ) { }
 
   getAlumnos(): Observable<Student[]> {
-    return this.http.get<Student[]>(`${this.baseUrl}/students`).pipe(delay(500));
+    return this.http.get<Student[]>(`${this.baseUrl}`);
   }
 
-  getAlumnoPorDni(dni: string): Observable<Student> {
-  return this.http.get<Student>(`${this.baseUrl}/students/${dni}`).pipe(delay(500));
+  getAlumnoPorId(id: string): Observable<Student> {
+  return this.http.get<Student>(`${this.baseUrl}/${id}`);
   }
 
   deleteAlumno(student: Student): Observable<void> {
-    return this.http.delete<void>(`${this.baseUrl}/students/${student.dni}`).pipe(delay(500));
+    return this.http.delete<void>(`${this.baseUrl}/${student.dni}`);
   }
 
   updateAlumno(student: Student): Observable<Student> {
-  return this.http.put<Student>(`${this.baseUrl}/students/${student.dni}`, student).pipe(delay(500));
+  return this.http.put<Student>(`${this.baseUrl}/${student.id}`, student);
   }
 
 }
