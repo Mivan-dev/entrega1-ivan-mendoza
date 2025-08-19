627ba5ec740e5f1d385a0623b79ab8e3e13184e8
Ivan-M <sirius.ivo@gmail.com>
2025-08-19 13:48:50 -0300

    fearure: add logic credentials guard

--- src/shared/guards/credentials-guard.ts
+++ src/shared/guards/credentials-guard.ts
@@ -1,21 +1,43 @@
-import { CanActivateFn } from '@angular/router';
-
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { Auth } from '../../app/core/auth/auth';
+import { map } from 'rxjs/operators';
+import { RoutesPaths } from '../routes';
 
 export const credentialsGuard: CanActivateFn = (route, state) => {
+  const auth = inject(Auth);
+  const router = inject(Router);
 
-  if(!this.user.loggedUser$){
-    return false;
-  }
-
-
-  return true;
+  return auth.loggedUser$.pipe(
+    map(user => {
+      if (!user) {
+        router.navigate(['/login']);
+        return false;
+      }
+      return true;
+    })
+  );
 };
 
 export const isAdminGuard: CanActivateFn = (route, state) => {
-  if (this.user.role === 'admin'){
-    return true;
-  } else {
-    this.router.navigate(['/not-authorized']);
-    return false
-  }
-}
+  const auth = inject(Auth);
+  const router = inject(Router);
+
+  return auth.loggedUser$.pipe(
+    map(user => {
+      if (!user) {
+        router.navigate([RoutesPaths.LOGIN]);
+        return false;
+      }
+      
+      if (user.role !== 'admin') {
+        router.navigate([RoutesPaths.ALUMNOS]);
+        // TODO: hacer vista de no autorizado, toast o simplemente el boton disable
+        console.error('Acceso denegado - Se requiere rol de administrador');
+        return false;
+      }
+      
+      return true;
+    })
+  );
+};
