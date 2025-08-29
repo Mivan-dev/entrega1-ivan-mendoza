b0e327be979ac08934bf4a41d4503c433d783714
Ivan-M <sirius.ivo@gmail.com>
2025-08-28 21:51:51 -0300

    feature: update buttons visibility based on permissions

--- src/app/feature/cursos/curso-tabla/curso-tabla.ts
+++ src/app/feature/cursos/curso-tabla/curso-tabla.ts
@@ -2,20 +2,34 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Course } from '../../../../shared/entities';
 import { Router, RouterModule } from '@angular/router';
 import { MatTableModule } from '@angular/material/table';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { selectIsAdmin } from '../../../ngrx/auth/auth.selectors';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { FullnamePipe } from '../../../../shared/pipes/fullname-pipe';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-curso-tabla',
-  imports: [MatTableModule, RouterModule],
+  imports: [MatTableModule, RouterModule, MatButtonModule, MatIconModule, FullnamePipe, CommonModule],
   templateUrl: './curso-tabla.html',
   styleUrl: './curso-tabla.scss'
 })
 export class CursoTabla {
+  
   @Input() courses: Course[] = [];
   @Output() deleteEvent = new EventEmitter<Course>();
 
+  isAdmin$: Observable<boolean>;
   displayedColumns: string[] = ['name', 'description', 'duration', 'actions'];
 
-  constructor (private router: Router) { }
+  constructor (
+    private router: Router,
+    private store: Store
+  ) {
+    this.isAdmin$ = this.store.select(selectIsAdmin);
+  }
 
   viewDetails(course: Course) {
     this.router.navigate(['/ver-curso'],{
