82c7f6b02def5d608eb645355481d554d3c7ca06
Ivan-M <sirius.ivo@gmail.com>
2025-08-29 18:17:26 -0300

    feature: update users

--- src/shared/entities.ts
+++ src/shared/entities.ts
@@ -29,5 +29,9 @@ export interface User {
 
 export const USERS_DATA: User[] = [
   { username: 'admin', password: 'admin123', role: 'admin' },
-  { username: 'user', password: 'user123', role: 'user' }
+  { username: 'user', password: 'user123', role: 'user' },
+  { username: 'guest', password: 'guest123', role: 'user' },
+  { username: 'Ivan', password: 'ivan123', role: 'admin' },
+  { username: 'Maria', password: 'maria123', role: 'user' },
+  { username: 'Pedro', password: 'pedro123', role: 'user' }
 ];
